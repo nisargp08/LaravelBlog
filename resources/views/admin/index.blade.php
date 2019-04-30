172e9ebf53ac9d3ed73c898a1984f77a016fa1a1
Nisarg Patel <nisargp42@gmail.com>
2019-04-30 12:00:23 -0400

    AdminPanel - SidebarDone

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,1 +1,4 @@
 @extends('layouts.sidebar')
+@section('pageContent')
+<h1>Admin - Dashboard</h1>
+@endsection
--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -7,7 +7,7 @@
             <ul class="sidebar-nav">
                 <li class="sidebar-brand">
                     <a href="#">
-                       Laravel Blogging System
+                      <b>{{ config('app.name') }}</b>
                     </a>
                 </li>
                 <li>
@@ -20,16 +20,14 @@
         <!-- Page Content -->
         <div id="page-content-wrapper">
             <div class="container-fluid">
-                <div class="row">
-                        <h1>Simple Sidebar</h1>
-                        <p>This template has a responsive menu toggling system. The menu will appear collapsed on smaller screens, and will appear non-collapsed on larger screens. When toggled using the button below, the menu will appear/disappear. On small screens, the page content will be pushed off canvas.</p>
-                        <p>Make sure to keep all page content within the <code>#page-content-wrapper</code>.</p>
+                    <div class="row">
+                        @yield('pageContent')
                     </div>
-                </div>
             </div>
         </div>
-        <!-- /#page-content-wrapper -->
-
     </div>
-    <!-- /#wrapper -->
+    <!-- /#page-content-wrapper -->
+
+</div>
+<!-- /#wrapper -->
 @endsection
