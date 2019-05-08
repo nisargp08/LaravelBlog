d96e44991f9e77b3ef6936e4b91a4d664f77abec
Nisarg Patel <nisargp42@gmail.com>
2019-05-08 15:07:50 -0400

    DisplayAllBlogPosts - Done
    Added posts link to navbar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -45,6 +45,9 @@
 
                     <!-- Right Side Of Navbar -->
                     <ul class="navbar-nav ml-auto">
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('blogposts.index') }}">Posts</a>
+                        </li>
                         <!-- Authentication Links -->
                         @guest
                             <li class="nav-item">
