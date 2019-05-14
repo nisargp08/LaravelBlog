1557ac317f1f2ae1448eb8edf8b5f52efb5efda9
Nisarg Patel <nisargp42@gmail.com>
2019-05-14 14:17:15 -0400

    Database Seeders
    Added few function on how to seed database using factory and faker data.Not fully implemented for the database.Just few examples on how to populate db.

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Database\Seeder;
-
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -12,5 +11,14 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        DB::statement('SET FOREIGN_KEY_CHECKS=0');
+        //DB::table('users')->truncate();
+        //DB::table('posts')->truncate();
+
+        /*Calling users factory*/
+        factory(App\User::class,2)->create()->each(function($user){
+            $user->post()->save(factory(App\Post::class)->make());
+        });
+
     }
 }
